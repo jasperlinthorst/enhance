102c072ebfcfd3d78553b794fd576a74b0e9bc7b
Jasper Linthorst <jasper.linthorst@gmail.com>
2016-03-07 15:41:10 +0100

    boost 1.60 for pacbio tools

---
+++ packages/boost-1.60.0.py
@@ -0,0 +1,15 @@
+from package import *
+
+class boost(Package):
+    fetch="http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.tar.gz"
+
+
+    config="""
+            cd ./libs/context/src/asm/
+            cp %(distpath)s/packages/boost_ppc64.patch .
+            patch -p0 < boost_ppc64.patch
+            cd ../../../../
+            ./bootstrap.sh --prefix=%(prefix)s
+            """
+
+    install = "./b2 install"
