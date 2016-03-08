6151f1650513c8dcc60a69b0bbf237971988fb64
jasperlinthorst <jasper.linthorst@gmail.com>
2016-03-08 15:41:55 +0100

    Added reveal dependency

--- packages/reveal-9999.py
+++ packages/reveal-9999.py
@@ -1,7 +1,7 @@
 from package import *
 
 class reveal(PythonPackage):
-    dependencies = ["git","libdivsufsort"]
+    dependencies = ["git","libdivsufsort","seqal_python"]
 
     def fetch(self):
         runCommand("git clone https://github.com/jasperlinthorst/reveal.git")
