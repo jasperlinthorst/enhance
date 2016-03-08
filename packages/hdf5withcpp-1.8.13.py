0205f9cf542ced25d373222bec51fa08525a7219
jasperlinthorst <jasper.linthorst@gmail.com>
2016-03-08 10:41:47 +0100

    updated link to hdf5 download

--- packages/hdf5withcpp-1.8.13.py
+++ packages/hdf5withcpp-1.8.13.py
@@ -2,6 +2,6 @@ from package import *
 
 class hdf5withcpp(MakePackage):
     dependencies = ["zlib"]
-    fetch="http://www.hdfgroup.org/ftp/HDF5/current/src/hdf5-1.8.13.tar.gz"
+    fetch="https://www.hdfgroup.org/ftp/HDF5/releases/hdf5-1.8.13/src/hdf5-1.8.13.tar.gz"
     config="./configure --prefix=%(prefix)s --enable-cxx"
 
